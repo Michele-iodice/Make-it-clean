a08907b23c2c56b94764dc0e9cf102aef010358e
Michele Iodice <M.iodice21@studenti.unisa.it>
2023-09-14 18:38:55 +0200

    ddhdhd

--- code/MakeItClean/language_model.py
+++ code/MakeItClean/language_model.py
@@ -3,7 +3,7 @@ import pandas as pd
 import os
 import re
 
-import tf as tf
+import tensorflow as tf
 from keras.utils import to_categorical
 from keras.preprocessing.sequence import pad_sequences
 from keras.models import Sequential
@@ -101,41 +101,41 @@ if __name__ == '__main__':
     # preprocess the text
     data_new = text_cleaner(data)
     # create sequences
-    sequences = create_seq(data_new)
+    #sequences = create_seq(data_new)
     # print(sequences)
     # create a character mapping index
     chars = sorted(list(set(data_new)))
     mapping = dict((c, i) for i, c in enumerate(chars))
     # encode the sequences
-    sequences = encode_seq(sequences)
+    #sequences = encode_seq(sequences)
     # print(sequences)
     # vocabulary size
-    vocab = len(mapping)
-    sequences = np.array(sequences)
+    #vocab = len(mapping)
+    #sequences = np.array(sequences)
     # create X and y
-    X, y = sequences[:, :-1], sequences[:, -1]
+    #X, y = sequences[:, :-1], sequences[:, -1]
     # one hot encode y
-    y = to_categorical(y, num_classes=vocab)
+    #y = to_categorical(y, num_classes=vocab)
     # create train and validation sets
-    X_tr, X_val, y_tr, y_val = train_test_split(X, y, test_size=0.1, random_state=42)
+    #X_tr, X_val, y_tr, y_val = train_test_split(X, y, test_size=0.1, random_state=42)
 
-    print('Train shape:', X_tr.shape, 'Val shape:', X_val.shape)
+    #print('Train shape:', X_tr.shape, 'Val shape:', X_val.shape)
 
     # define model
-    model = Sequential()
-    model.add(Embedding(vocab, 50, input_length=30, trainable=True))
-    model.add(GRU(150, recurrent_dropout=0.1, dropout=0.1))
-    model.add(Dense(vocab, activation='softmax'))
-    print(model.summary())
+    #model = Sequential()
+    #model.add(Embedding(vocab, 50, input_length=30, trainable=True))
+    #model.add(GRU(150, recurrent_dropout=0.1, dropout=0.1))
+    #model.add(Dense(vocab, activation='softmax'))
+    #print(model.summary())
 
     # compile the model
-    model.compile(loss='categorical_crossentropy', metrics=['acc'], optimizer='adam')
+    #model.compile(loss='categorical_crossentropy', metrics=['acc'], optimizer='adam')
     # fit the model
-    model.fit(X_tr, y_tr, epochs=100, verbose=2, validation_data=(X_val, y_val))
+    #model.fit(X_tr, y_tr, epochs=100, verbose=2, validation_data=(X_val, y_val))
     # save the model
-    model.save('Models/language_model_1')
+    #model.save('Models/language_model_1')
     # load the model
-    loaded_model = tf.keras.models.load_model('language_model_1')
-    inp="large size of"
+    loaded_model = tf.keras.models.load_model('Models/language_model_1')
+    inp="large armies for "
     print(len(inp))
     print(generate_seq(loaded_model, mapping, 30, inp.lower(), 15))
